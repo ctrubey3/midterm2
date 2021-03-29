c39f632bf5b1efd413b467c6998a20a4390999aa
ctrubey3 <70168073+ctrubey3@users.noreply.github.com>
2021-03-29 16:55:43 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
 import './App.css';
-import Nav from "./components/core/Nav";
-import All from "./components/core/All";
-import Animation from "./components/core/Animation";
-import Games from "./components/core/Games";
-import Television from "./components/core/Television";
-import Goku from "./components/Cards/Goku";
-import Vegeta from "./components/Cards/Vegeta";
+import React, { Component } from 'react'
 import {BrowserRouter as Router, Switch, Route,} from "react-router-dom";
+import {data} from "./data";
+import { v4 as uuidv1 } from 'uuid';
 
 
-function App() {
-  return (
-      <Router>
-        <div className="App">
-          <Nav/>
-          <Switch>
-            <Route exact path={"/"} component={All}/>
-            <Route exact path={"/Animation"} component={Animation}/>
-            <Route exact path={"/Games"} component={Games}/>
-            <Route exact path={"/Television"} component={Television}/>
-            <Route exact path={"/Goku"} component={Goku}/>
-            <Route exact path={"/Vegeta"} component={Vegeta}/>
+import All from "./components/Pages/All";
+import Animation from "./components/Pages/Animation";
+import Games from "./components/Pages/Games";
+import Television from "./components/Pages/Television";
+import UserInfo from "./components/Pages/UserInfo";
 
-          </Switch>
-        </div>
-      </Router>
-  );
+
+
+import Box from "./components/Box";
+import Nav from "./components/Nav";
+
+class App extends Component {
+    state = {
+        data: data,
+        name: '',
+        category: '',
+        value: '',
+        id: uuidv1(),
+        img: '',
+        search: '',
+
+    }
+    getFilterData = () => {
+        return this.state.data.filter(category =>
+            category.name.toLowerCase().includes(this.state.search.toLowerCase())
+        )
+    }
+
+
+
+
+
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Nav/>
+                    <Switch>
+                        <Route exact path={"/"} component={All}/>
+                        <Route exact path={"/Animation"} component={Animation}/>
+                        <Route exact path={"/Games"} component={Games}/>
+                        <Route exact path={"/Television"} component={Television}/>
+                        <Route exact path={"/UserInfo/:id"} component={UserInfo}/>
+                        <Box getFilterData={this.getFilterData}/>
+                    </Switch>
+                </div>
+
+
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Box.js
@@ -0,0 +1,30 @@
+import React from 'react'
+const Box = props => {
+    const {getFilterData} = props
+
+
+    return (
+        <div>
+
+            <ul className="c-list__Funks">
+                {getFilterData().map(item => {
+                        return (
+
+                            <li className="c-list__Funk" key={item.name}>
+                                    <h5>Name:{item.name}</h5>
+                                    <h5>Category:{item.category}</h5>
+                                    <h5>Value{item.value}</h5>
+                                    <h5>id:{item.id}-</h5>
+                                    <img src={item.img} width="100" alt="item.img"/>
+
+                            </li>
+                        )
+                    }
+
+                )}
+            </ul>
+        </div>
+    )
+}
+
+export default Box
